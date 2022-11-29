7bc24d6b5af0440c931ec98a5229fcea09eef459
Dimitar Rusev <mitikodev@gmail.com>
2022-11-29 18:59:13 +0200

    Color multiple contexts

--- content/tech/compression/fsm/aux/script.py
+++ content/tech/compression/fsm/aux/script.py
@@ -2,29 +2,28 @@
 
 import re
 
-out_annotated = open("book1-annotated", "w")
-out_postfixes = open("book1-postfixes", "w")
+for ctx in ["th", "an", "the"]:
+    out_annotated = open(f"book1-{ctx}-annotated", "w")
+    out_postfixes = open(f"book1-{ctx}-postfixes", "w")
 
-# ctx = "th"
-ctx = "an"
-postfix_list = [] # use an ordered set for bigger files
+    postfix_list = [] # use an ordered set for bigger files
 
-# we match the context, and capture the postfix character
-pattern = rf'{ctx}(.)'
+    # we match the context, and capture the postfix character
+    pattern = rf'{ctx}(.)'
 
-for line in open("book1"):    
-    def _sub(match):
-        postfix = match.group(1)
-        if postfix not in postfix_list:
-            postfix_list.append(postfix)
+    for line in open("book1"):    
+        def _sub(match):
+            postfix = match.group(1)
+            if postfix not in postfix_list:
+                postfix_list.append(postfix)
 
-        # the stylesheet defines colors for _0, _1, _2, _3,
-        # others will remain blank
-        id = postfix_list.index(postfix)
-        repl = f'<span class="_{id}">{postfix}</span>'
+            # the stylesheet defines colors for _0, _1, _2, _3,
+            # others will remain blank
+            id = postfix_list.index(postfix)
+            repl = f'<span class="_{id}">{postfix}</span>'
 
-        out_postfixes.write(repl)
-        return f'<span class="ctx">{ctx}</span>{repl}'
+            out_postfixes.write(repl)
+            return f'<span class="ctx">{ctx}</span>{repl}'
 
-    annotated_line = re.sub(pattern, _sub, line)
-    out_annotated.write(annotated_line)
+        annotated_line = re.sub(pattern, _sub, line)
+        out_annotated.write(annotated_line)
