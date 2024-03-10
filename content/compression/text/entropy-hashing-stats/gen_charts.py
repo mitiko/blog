60cf06d6844a5da6f36e2d573b38ed70e7c6a727
Dimitar Rusev <mitikodev@gmail.com>
2024-03-10 14:33:14 +0200

    Add ruff.toml formatting

--- content/compression/text/entropy-hashing-stats/gen_charts.py
+++ content/compression/text/entropy-hashing-stats/gen_charts.py
@@ -1,18 +1,54 @@
 #!/usr/bin/env python3
 
 import re
+
 data = []
 
-# parse ac-over-huff
+
+def parse_nanos(s: str):
+    if s.endswith('ns'):
+        return float(s[:-2])
+    if s.endswith('ms'):
+        return float(s[:-2]) * 1_000
+    if s.endswith('s'):
+        return float(s[:-1]) * 1_000 * 1_000
+    raise Exception('Unknown time unit')
+
 
 # [ac-over-huff] [hsize:  7 ctx: 11 align: 0] csize: 388436 (ratio: 0.505), ctime: 54.287041ms (9ns per bit)
-pat = re.compile('\[ac-over-huff\] \[hsize:\s+(\d+) ctx:\s+(\d+) align: 0\] csize: (\d+) \(ratio: ([\d\.]+)\), ctime: ([\d\w\.]+) \(([\w\d]+) per bit\)')
-for line in open('ac-over-huff.book1.log').read().split('\n'):
-    if not line.startswith('[ac-over-huff]'):
-        pass
-    x = pat.match(line)
-    print(x.groups())
-    break
-    pass
+patterns = {
+    'ac-over-huff': re.compile(
+        r'\[ac-over-huff\] '
+        r'\[hsize:\s+(?P<hsize>\d+) ctx:\s+(?P<ctx>\d+) align: 0\] '
+        r'csize: (?P<csize>\d+) \(ratio: (?P<ratio>[\d\.]+)\), '
+        r'ctime: (?P<ctime>[\d\w\.]+) \((?P<btime>[\w\d]+) per bit\)'
+    )
+}
 
-print("hello world")
+parse_funcs = {
+    'ac-over-huff': {
+        'hsize': int,
+        'ctx': int,
+        'csize': int,
+        'ratio': float,
+        'ctime': parse_nanos,
+        'btime': parse_nanos,
+    }
+}
+
+for alg in patterns.keys():
+    for line in open(f'{alg}.book1.log').read().split('\n'):
+        x = patterns[alg].match(line)
+        if x is None:
+            continue
+        obj = x.groupdict()
+        for key, func in parse_funcs[alg].items():
+            obj[key] = func(obj[key])
+        obj['alg'] = alg
+        data.append(obj)
+
+print(data[0])
+print(data[1])
+print(data[2])
+
+print('hello world')
