df54d549e2b8106f8bc95e173051d80b21d8a96e
Dimitar Rusev <mitikodev@gmail.com>
2024-03-10 15:06:09 +0200

    Parse all files

--- content/compression/text/entropy-hashing-stats/gen_charts.py
+++ content/compression/text/entropy-hashing-stats/gen_charts.py
@@ -11,32 +11,49 @@ def parse_nanos(s: str):
     if s.endswith('ms'):
         return float(s[:-2]) * 1_000
     if s.endswith('s'):
-        return float(s[:-1]) * 1_000 * 1_000
+        return float(s[:-1]) * 1_000_000
     raise Exception('Unknown time unit')
 
 
-# [ac-over-huff] [hsize:  7 ctx: 11 align: 0] csize: 388436 (ratio: 0.505), ctime: 54.287041ms (9ns per bit)
 patterns = {
+    # [ac-over-huff] [hsize:  7 ctx: 11 align: 0] csize: 388436 (ratio: 0.505), ctime: 54.287041ms (9ns per bit)
     'ac-over-huff': re.compile(
-        r'\[ac-over-huff\] '
+        r'^\[ac-over-huff\] '
         r'\[hsize:\s+(?P<hsize>\d+) ctx:\s+(?P<ctx>\d+) align: 0\] '
         r'csize: (?P<csize>\d+) \(ratio: (?P<ratio>[\d\.]+)\), '
-        r'ctime: (?P<ctime>[\d\w\.]+) \((?P<btime>[\w\d]+) per bit\)'
-    )
+        r'ctime: (?P<ctime>[\w\.]+) \((?P<btime>[\w]+) per bit\)$'
+    ),
+    # [eh-ac] [ctx:  8, align: 0] csize: 687637 (ratio 0.894), ctime: 876.99425ms (143ns per bit)
+    'eh-ac': re.compile(
+        r'^\[eh-ac\] '
+        r'\[ctx:\s+(?P<ctx>\d+), align:\s+(?P<align>\d+)\] '
+        r'csize: (?P<csize>\d+) \(ratio (?P<ratio>[\d\.]+)\), '
+        r'ctime: (?P<ctime>[\w\.]+) \((?P<btime>[\w\.]+) per bit\)$'
+    ),
+    # [eh-huff] [rem_hsize:  7, hsize:  7, ctx:  8] csize: 414561 (ratio: 0.539), ctime: 118.488417ms (19ns per bit)
+    'eh-huff': re.compile(
+        r'^\[eh-huff\] '
+        r'\[rem_hsize:\s+(?P<rem_hsize>\d+), hsize:\s+(?P<hsize>\d+), ctx:\s+(?P<ctx>\d+)\] '
+        r'csize: (?P<csize>\d+) \(ratio: (?P<ratio>[\d\.]+)\), '
+        r'ctime: (?P<ctime>[\w\.]+) \((?P<btime>[\w\.]+) per bit\)$'
+    ),
 }
 
+algs = ['ac-over-huff', 'eh-ac', 'eh-huff']
 parse_funcs = {
-    'ac-over-huff': {
-        'hsize': int,
+    alg: {
         'ctx': int,
         'csize': int,
         'ratio': float,
         'ctime': parse_nanos,
         'btime': parse_nanos,
     }
+    for alg in algs
 }
+parse_funcs['ac-over-huff'].update({'hsize': int})
+parse_funcs['eh-ac'].update({'align': int})
 
-for alg in patterns.keys():
+for alg in algs:
     for line in open(f'{alg}.book1.log').read().split('\n'):
         x = patterns[alg].match(line)
         if x is None:
@@ -47,8 +64,9 @@ for alg in patterns.keys():
         obj['alg'] = alg
         data.append(obj)
 
-print(data[0])
-print(data[1])
-print(data[2])
-
-print('hello world')
+x1 = [x for x in data if x['alg'] == algs[0]][0]
+x2 = [x for x in data if x['alg'] == algs[1]][0]
+x3 = [x for x in data if x['alg'] == algs[2]][0]
+print(x1)
+print(x2)
+print(x3)
